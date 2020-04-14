adcfd8c6301350679646940a00970d7455b350c5
Chas3-Mahat <56127094+Chas3-Mahat@users.noreply.github.com>
2020-04-14 10:22:09 +0100

    edited item_orders table & added test data

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -8,3 +8,7 @@ insert into ims.items(item_name, price) values('Angels and Demons', 7.99);
 insert into ims.items(item_name, price) values('A Tale of Two Cities', 4.99);
 insert into ims.items(item_name, price) values('Never Gonna: Rick Rolled', 4.99);
 
+insert into ims.item_orders(order_id, item_id, qty) values(1, 4, 2);
+insert into ims.item_orders(order_id, item_id, qty) values(1, 1);
+
+insert into ims.orders(customer_id, total) values(1, 14.97);
