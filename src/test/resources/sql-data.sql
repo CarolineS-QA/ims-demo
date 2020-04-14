3d9540465ab6746c9882baf33d673b279c71d9b8
Chas3-Mahat <56127094+Chas3-Mahat@users.noreply.github.com>
2020-04-14 17:39:54 +0100

    added stock to items

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -2,13 +2,12 @@ insert into ims.customers(first_name, surname) values('chris', 'perrins');
 insert into ims.customers(first_name, surname) values('rhys', 'thompson');
 insert into ims.customers(first_name, surname) values('nic', 'johnson');
 insert into ims.customers(first_name, surname) values('jordon', 'harrison');
-
-insert into ims.items(item_name, price) values('H is for Hawk', 4.99);
-insert into ims.items(item_name, price) values('Angels and Demons', 7.99);
-insert into ims.items(item_name, price) values('A Tale of Two Cities', 4.99);
-insert into ims.items(item_name, price) values('Never Gonna: Rick Rolled', 4.99);
-
+insert into ims.items(item_name, price, stock) values('H is for Hawk', 4.99, 40);
+insert into ims.items(item_name, price, stock) values('Angels and Demons', 7.99, 20);
+insert into ims.items(item_name, price, stock) values('A Tale of Two Cities', 4.99, 20);
+insert into ims.items(item_name, price, stock) values('Never Gonna: Rick Rolled', 4.99);
+insert into ims.orders(customer_id, total) values(1, 14.97);
+insert into ims.orders(customer_id, total) values(2, 7.99);
 insert into ims.item_orders(order_id, item_id, qty) values(1, 4, 2);
 insert into ims.item_orders(order_id, item_id, qty) values(1, 1);
-
-insert into ims.orders(customer_id, total) values(1, 14.97);
+insert into ims.item_orders(order_id, item_id, qty) values(2, 2, 1);
