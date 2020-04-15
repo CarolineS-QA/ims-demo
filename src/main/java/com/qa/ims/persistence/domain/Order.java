561759ec57763de628abb441a38fb8f2913ac9bc
Chas3-Mahat <56127094+Chas3-Mahat@users.noreply.github.com>
2020-04-15 14:31:13 +0100

    added vars and constructors for Orders to incorp Orderline

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -1,6 +1,7 @@
 package com.qa.ims.persistence.domain;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public class Order {
 
@@ -8,10 +9,10 @@ public class Order {
 	private Long customerId;
 	private BigDecimal total;
 	// item_orders
-	private Long itemId;
-	private Integer qty;
+	private ArrayList<Long> items;
+	private ArrayList<Integer> qty;
 
-	// for order table
+	// for SQL order table
 	public Order(Long customerId, BigDecimal total) {
 		this.customerId = customerId;
 		this.total = total;
@@ -24,16 +25,25 @@ public class Order {
 	}
 
 	// for item_orders table
-	public Order(Long orderId, Long itemId, Integer qty) {
+	public Order(Long orderId, ArrayList<Long> items, ArrayList<Integer> qty) {
 		this.orderId = orderId;
-		this.itemId = itemId;
+		this.items = items;
 		this.qty = qty;
 	}
 
-	public Order(Long orderId, Long customerId, Long itemId, Integer qty) {
+	// new order constructors
+	public Order(Long customerId, BigDecimal total, ArrayList<Long> items, ArrayList<Integer> qty) {
+		this.customerId = customerId;
+		this.total = total;
+		this.items = items;
+		this.qty = qty;
+	}
+
+	public Order(Long orderId, Long customerId, BigDecimal total, ArrayList<Long> items, ArrayList<Integer> qty) {
 		this.orderId = orderId;
 		this.customerId = customerId;
-		this.itemId = itemId;
+		this.total = total;
+		this.items = items;
 		this.qty = qty;
 	}
 
@@ -61,75 +71,26 @@ public class Order {
 		this.total = total;
 	}
 
-	public Long getItemId() {
-		return itemId;
+	public ArrayList<Long> getItems() {
+		return items;
 	}
 
-	public void setItemId(Long itemId) {
-		this.itemId = itemId;
+	public void setItems(ArrayList<Long> items) {
+		this.items = items;
 	}
 
-	public Integer getQty() {
+	public ArrayList<Integer> getQty() {
 		return qty;
 	}
 
-	public void setQty(Integer qty) {
+	public void setQty(ArrayList<Integer> qty) {
 		this.qty = qty;
 	}
 
 	@Override
 	public String toString() {
-		return "Order id: " + orderId + " |\t Customer id: " + customerId + " |\tTotal: " + total + "\nItemId: "
-				+ itemId + " |\t Qty: " + qty;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((customerId == null) ? 0 : customerId.hashCode());
-		result = prime * result + ((itemId == null) ? 0 : itemId.hashCode());
-		result = prime * result + ((orderId == null) ? 0 : orderId.hashCode());
-		result = prime * result + ((qty == null) ? 0 : qty.hashCode());
-		result = prime * result + ((total == null) ? 0 : total.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Order other = (Order) obj;
-		if (customerId == null) {
-			if (other.customerId != null)
-				return false;
-		} else if (!customerId.equals(other.customerId))
-			return false;
-		if (itemId == null) {
-			if (other.itemId != null)
-				return false;
-		} else if (!itemId.equals(other.itemId))
-			return false;
-		if (orderId == null) {
-			if (other.orderId != null)
-				return false;
-		} else if (!orderId.equals(other.orderId))
-			return false;
-		if (qty == null) {
-			if (other.qty != null)
-				return false;
-		} else if (!qty.equals(other.qty))
-			return false;
-		if (total == null) {
-			if (other.total != null)
-				return false;
-		} else if (!total.equals(other.total))
-			return false;
-		return true;
+		return "Order id: " + orderId + " |\t Customer id: " + customerId + " |\tTotal: " + total + "\nItems: " + items
+				+ " |\t Qty: " + qty;
 	}
 
 }
