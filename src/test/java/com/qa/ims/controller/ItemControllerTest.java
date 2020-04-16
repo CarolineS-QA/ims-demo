8bd036c50a466ee52aa738f1247ffe5d75bfd89c
Chas3-Mahat <56127094+Chas3-Mahat@users.noreply.github.com>
2020-04-16 11:25:32 +0100

    created empty Test classes for Item/Order Controller and Dao

---
+++ src/test/java/com/qa/ims/controller/ItemControllerTest.java
@@ -0,0 +1,9 @@
+package com.qa.ims.controller;
+
+public class ItemControllerTest {
+
+	public ItemControllerTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/test/java/com/qa/ims/persistence/dao/ItemDaoMysqlTest.java
@@ -0,0 +1,9 @@
+package com.qa.ims.persistence.dao;
+
+public class ItemDaoMysqlTest {
+
+	public ItemDaoMysqlTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
